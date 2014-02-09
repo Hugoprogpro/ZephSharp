b65613b03ba34d4799d7bd254bac31003cfc94f9
Steven Degutis <steven@cleancoders.com>
2014-02-09 13:12:02 -0600

    lotsa refactoring!

---
+++ Zephyros/HotKey.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Zephyros
+{
+    public class HotKey
+    {
+        [DllImport("user32", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private KeyModifiers translateMods(IEnumerable<string> mods)
+        {
+            KeyModifiers keyMods = 0;
+            mods = mods.Select(mod => mod.ToUpper()).ToList();
+            if (mods.Contains("ALT")) keyMods |= KeyModifiers.Alt;
+            if (mods.Contains("CTRL")) keyMods |= KeyModifiers.Control;
+            if (mods.Contains("SHIFT")) keyMods |= KeyModifiers.Shift;
+            if (mods.Contains("WINDOWS")) keyMods |= KeyModifiers.Windows;
+            if (mods.Contains("NOREPEAT")) keyMods |= KeyModifiers.NoRepeat;
+            return keyMods;
+        }
+
+        [Flags]
+        public enum KeyModifiers
+        {
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            Windows = 8,
+            NoRepeat = 0x4000
+        }
+
+        private static int nextHotkeyID = 0;
+        private int hotkeyID;
+
+        public bool OnHotKeyPressed()
+        {
+            Console.WriteLine("called!!");
+            return false;
+        }
+
+        public HotKey(string key, IEnumerable<string> mods)
+        {
+            uint rawMods = (uint)translateMods(mods);
+            uint rawKey = key.ToUpper().First();
+            IntPtr hwnd = CallbackWindow.SharedCallbackWindow.Handle;
+
+            hotkeyID = ++nextHotkeyID;
+            RegisterHotKey(hwnd, hotkeyID, rawMods, rawKey);
+            CallbackWindow.SharedCallbackWindow.HotKeyTable.Add(hotkeyID, this);
+        }
+
+        private class CallbackWindow : Form
+        {
+            static public readonly CallbackWindow SharedCallbackWindow = new CallbackWindow();
+
+            public readonly Dictionary<int, HotKey> HotKeyTable = new Dictionary<int, HotKey>();
+
+            protected override void SetVisibleCore(bool value)
+            {
+                base.SetVisibleCore(false);
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                Console.WriteLine("got here");
+                if (m.Msg == WM_HOTKEY)
+                {
+                    HotKey hotkey = HotKeyTable[m.WParam.ToInt32()];
+                    bool handled = hotkey.OnHotKeyPressed();
+                }
+
+                base.WndProc(ref m);
+            }
+
+            private const int WM_HOTKEY = 0x312;
+        }
+    }
+}
---
+++ Zephyros/Window.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace Zephyros
+{
+    class Window
+    {
+        private delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+        [DllImport("user32.dll", ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool EnumWindows(EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RawRect lpRect);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, ExactSpelling = true, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool MoveWindow(IntPtr hwnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RawRect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        private IntPtr ptr;
+
+        public static List<Window> GetWindows()
+        {
+            var windows = new List<Window>();
+            EnumDelegate filter = delegate(IntPtr hWnd, int lParam)
+            {
+                if (IsWindowVisible(hWnd))
+                {
+                    var w = new Window();
+                    w.ptr = hWnd;
+                    windows.Add(w);
+                }
+                return true;
+            };
+
+            EnumWindows(filter, IntPtr.Zero);
+            return windows;
+        }
+
+        public bool IsVisible()
+        {
+            return IsWindowVisible(ptr) && this.Title() != "";
+        }
+
+        public string Title()
+        {
+            StringBuilder strbTitle = new StringBuilder(255);
+            int nLength = GetWindowText(ptr, strbTitle, strbTitle.Capacity + 1);
+            string strTitle = strbTitle.ToString();
+
+            if (!string.IsNullOrEmpty(strTitle))
+                return strTitle;
+            else
+                return "";
+        }
+
+        public Rectangle GetRect()
+        {
+            RawRect rct;
+
+            if (!GetWindowRect(ptr, out rct))
+                return new Rectangle();
+
+            return new Rectangle { X = rct.Left, Y = rct.Top, Width = rct.Right - rct.Left + 1, Height = rct.Bottom - rct.Top + 1 };
+        }
+
+        public bool Move(Rectangle r)
+        {
+            return MoveWindow(ptr, r.X, r.Y, r.Width, r.Height, true);
+        }
+    }
+}
